c82baa35aaddec6631724a8b82a3f54ef5a0cbea
Joaquin Bejar <jb@taunais.com>
2023-12-09 20:46:04 +0100

    VERSION: 0.1.0

--- cmake/redis-plus-plus.cmake
+++ cmake/redis-plus-plus.cmake
@@ -2,8 +2,23 @@ include(FetchContent)
 FetchContent_Declare(rediscpp
         GIT_REPOSITORY https://github.com/sewenew/redis-plus-plus.git
         GIT_TAG 1.3.10
+#        CMAKE_ARGS -DREDIS_PLUS_PLUS_USE_TLS=OFF -DREDIS_PLUS_PLUS_CXX_STANDARD=17 -DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_BUILD_STATIC=ON -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF
         BUILD_ALWAYS 1
 )
+#FetchContent_MakeAvailable(rediscpp)
+#
+#set(REDISCPP_INCLUDE "${rediscpp_SOURCE_DIR}/src" CACHE INTERNAL "")
+#set(REDISCPP_HEADER "${rediscpp_SOURCE_DIR}/src" CACHE INTERNAL "")
+#set(REDISCPP_HEADER17 "${rediscpp_SOURCE_DIR}/src/sw/redis++/cxx17" CACHE INTERNAL "")
+#set(REDISCPP_HEADERTLS "${rediscpp_SOURCE_DIR}/src/sw/redis++/no_tls" CACHE INTERNAL "")
+#set(REDISCPP_LIB "${rediscpp_BINARY_DIR}/libredis++.a" CACHE INTERNAL "")
+#set(TEST_REDISCPP_LIB "${rediscpp_BINARY_DIR}/libredis++.a" CACHE INTERNAL "")
+#
+#if (CMAKE_DEBUG)
+#    message(STATUS "simple_redis/cmake rediscpp_SOURCE_DIR ${rediscpp_SOURCE_DIR}")
+#    message(STATUS "simple_redis/cmake REDISCPP_INCLUDE ${REDISCPP_INCLUDE}")
+#endif ()
+
 
 FetchContent_Populate(rediscpp)
 
@@ -30,6 +45,19 @@ if(NOT EXISTS "${REDISCPP_LIB}")
     if (result)
         message(FATAL_ERROR "Error building rediscpp ${result}")
     endif ()
+
+#    execute_process(
+#            COMMAND ln -sf ${rediscpp_SOURCE_DIR} ${rediscpp_SOURCE_DIR}/src/sw/redis++/cxx_utils.h
+#            WORKING_DIRECTORY ${hiredis_SOURCE_DIR}
+#            COMMAND_ECHO STDOUT
+#            COMMAND_ECHO STDERR
+#            RESULT_VARIABLE result
+#    )
+#    if (result)
+#        message(FATAL_ERROR "Error linking cxx_utils.h ${result}")
+#    endif ()
+
+
 else()
     message(STATUS "rediscpp library found")
 endif()
@@ -44,6 +72,8 @@ find_library(REDISCPP_LIB
 
 set(REDISCPP_INCLUDE "${rediscpp_SOURCE_DIR}/src" CACHE INTERNAL "")
 set(REDISCPP_HEADER "${rediscpp_SOURCE_DIR}/src" CACHE INTERNAL "")
+set(REDISCPP_HEADER17 "${rediscpp_SOURCE_DIR}/src/sw/redis++/cxx17" CACHE INTERNAL "")
+set(REDISCPP_HEADERTLS "${rediscpp_SOURCE_DIR}/src/sw/redis++/no_tls" CACHE INTERNAL "")
 
 if (CMAKE_DEBUG)
     message(STATUS "simple_redis/cmake rediscpp_SOURCE_DIR ${rediscpp_SOURCE_DIR}")
