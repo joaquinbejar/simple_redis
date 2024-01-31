d5a54b1651b5e2d7a4c20c13a7ea3bb5b398699d
Joaquin Bejar <jb@taunais.com>
2024-01-31 15:55:26 +0100

    VERSION: 0.1.3 update logger

--- cmake/redis-plus-plus.cmake
+++ cmake/redis-plus-plus.cmake
@@ -2,23 +2,8 @@ include(FetchContent)
 FetchContent_Declare(rediscpp
         GIT_REPOSITORY https://github.com/sewenew/redis-plus-plus.git
         GIT_TAG 1.3.10
-#        CMAKE_ARGS -DREDIS_PLUS_PLUS_USE_TLS=OFF -DREDIS_PLUS_PLUS_CXX_STANDARD=17 -DREDIS_PLUS_PLUS_BUILD_TEST=OFF -DREDIS_PLUS_PLUS_BUILD_STATIC=ON -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF
         BUILD_ALWAYS 1
 )
-#FetchContent_MakeAvailable(rediscpp)
-#
-#set(REDISCPP_INCLUDE "${rediscpp_SOURCE_DIR}/src" CACHE INTERNAL "")
-#set(REDISCPP_HEADER "${rediscpp_SOURCE_DIR}/src" CACHE INTERNAL "")
-#set(REDISCPP_HEADER17 "${rediscpp_SOURCE_DIR}/src/sw/redis++/cxx17" CACHE INTERNAL "")
-#set(REDISCPP_HEADERTLS "${rediscpp_SOURCE_DIR}/src/sw/redis++/no_tls" CACHE INTERNAL "")
-#set(REDISCPP_LIB "${rediscpp_BINARY_DIR}/libredis++.a" CACHE INTERNAL "")
-#set(TEST_REDISCPP_LIB "${rediscpp_BINARY_DIR}/libredis++.a" CACHE INTERNAL "")
-#
-#if (CMAKE_DEBUG)
-#    message(STATUS "simple_redis/cmake rediscpp_SOURCE_DIR ${rediscpp_SOURCE_DIR}")
-#    message(STATUS "simple_redis/cmake REDISCPP_INCLUDE ${REDISCPP_INCLUDE}")
-#endif ()
-
 
 FetchContent_Populate(rediscpp)
 
